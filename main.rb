02fae860243816c87f2c9560f215464bd9e151f8
向井優一 <yuichi-m@weseek.co.jp>
2021-07-29 15:28:35 +0900

    add monster class

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 class Brave
   #attr_readerによりゲッターを省略できる
-  attr_reader :name, :hp, :offense, :defense
+  attr_reader :name, :offense, :defense
   #セッターとゲッターを一括定義
   attr_accessor :hp
 
@@ -15,8 +15,25 @@ class Brave
 
 end
 
+class Monster
+  #値の取り出しのみ可能
+  attr_reader :name, :offense, :defense
+  #値の代入・取り出しが可能
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
 #インスタンスごとにパラメーターを自由に設定できる
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+
+#ハッシュ形式でデータを渡すためどういうデータを渡しているのかの把握がしやすい
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 #ヒアドキュメントによる記述
 puts <<~TEXT
